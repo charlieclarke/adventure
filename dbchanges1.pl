407041e503fa9fa48f9a41b23b5b28cc69c03681
EC2 Default User <ec2-user@ip-10-48-42-129.eu-west-1.compute.internal>
2012-06-17 21:03:32 +0000

    making safe

--- dbchanges1.pl
+++ dbchanges1.pl
@@ -26,20 +26,8 @@ my $db = DBI->connect("dbi:SQLite:$db_location", "", "",
 
 $db->do("INSERT INTO Action VALUES (10,'DialTone response', 'Number is irrelevant for this thread ',' ','This action describes how to respond to dial tones/ key presses. ')");
 
-$db->do("INSERT INTO Thread VALUES (3, '1 off call Charlies Phone', 2,'1','hello.mp3',0,0,23,59,0,0)");
-$db->do("INSERT INTO Thread VALUES (1, 'default inbound call behaviour', 5,'1','hello [InboundName]',0,0,23,59,0,0)");
-$db->do("INSERT INTO Thread VALUES (2, 'default inbound SMS behaviour', 8,'1','',0,0,23,59,0,0)");
 my $all = $db->selectall_arrayref("SELECT * FROM Thread");
 
-
-$db->do("DROP TABLE IF EXISTS TimeLine");
-$db->do("CREATE TABLE TimeLine (id INTEGER PRIMARY KEY, ThreadId INTEGER, ActivityTime DATETIME, Completed INTEGER, CompletedTime DATETIME, Description TEXT, Notes TEXT, AdditionalNumberID INTEGER)");
-
-
-foreach my $row (@$all) {
-my ($id, $first, $last) = @$row;
-print "$id|$first|$lastn";
-}
 
 
 
